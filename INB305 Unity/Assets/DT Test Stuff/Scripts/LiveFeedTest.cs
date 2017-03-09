3b13dbca9a3b8a7121a4be75028425735b7f81c7
Daniel Tan <danieltan843@gmail.com>
2017-03-09 21:53:00 +1000

    Test Stream webcam

---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/LiveFeedTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveFeedTest : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for (int i = 0; i < devices.Length; i++) {
+			Debug.Log (devices [i].name);
+		}
+	}
+}
