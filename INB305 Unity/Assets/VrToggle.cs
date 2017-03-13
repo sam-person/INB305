95d03a3e2dd1f7d8feb084d66039840ff8a6d4e3
Vaconcovat <vaconcovat@gmail.com>
2017-03-13 14:57:05 +1000

    Controller tracking and non-vr mode
    Added text in the world to display controller co-ordinates (as if we
    were tracking cars)
    Added a prompt on startup to switch between vr and FPS mode
    FPS mode may require steamvr to be running, but no HMD is required

---
+++ INB305 Unity/Assets/VrToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VrToggle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject VRCharacter;
+	[SerializeField]
+	private GameObject FPSCharacter;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.V)){
+			//VRSettings.LoadDeviceByName("OpenVR");
+			//VRCharacter.SetActive(true);
+			//FPSCharacter.SetActive(false);
+			Destroy(this.gameObject);
+		}
+		if(Input.GetKeyDown(KeyCode.F)){
+			VRSettings.LoadDeviceByName("None");
+			VRCharacter.SetActive(false);
+			FPSCharacter.SetActive(true);
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ INB305 Unity/Assets/_Main/Scripts/ControllerStatsDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControllerStatsDisplay : MonoBehaviour {
+
+	public TextMeshPro textMesh;
+
+	private string _text;
+
+	[SerializeField]
+	private Transform controller1;
+	[SerializeField]
+	private Transform controller2;
+
+	// Update is called once per frame
+	void Update () {
+		_text = "Controller 1: " + controller1.position.x.ToString("F3") + ", " + controller1.position.y.ToString("F3") + ", " + controller1.position.z.ToString("F3")
+			+ "\nController 2: " + controller2.position.x.ToString("F3") + ", " + controller2.position.y.ToString("F3") + ", " + controller2.position.z.ToString("F3");
+
+		textMesh.SetText(_text);
+	}
+}
