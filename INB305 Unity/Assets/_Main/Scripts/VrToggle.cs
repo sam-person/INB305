9326c3d22fc3c9b845e7d25e86813d05a36263f6
Daniel Tan <danieltan843@gmail.com>
2017-03-16 21:21:23 +1000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	INB305 Unity/Assets/_Main/_Scenes/VR Test scene.unity
    #	INB305 Unity/Library/CurrentLayout.dwlt
    #	INB305 Unity/Library/InspectorExpandedItems.asset
    #	INB305 Unity/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	INB305 Unity/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	INB305 Unity/Library/ShaderCache.db
    #	INB305 Unity/Library/assetDatabase3
    #	INB305 Unity/Library/expandedItems
    #	INB305 Unity/Library/metadata/00/00000000000000004000000000000000
    #	INB305 Unity/Library/metadata/00/0000000000000000b000000000000000

---
+++ INB305 Unity/Assets/_Main/Scripts/VrToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using TMPro;
+
+public class VrToggle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject VRCharacter;
+	[SerializeField]
+	private GameObject FPSCharacter;
+	[SerializeField]
+	private TextMeshProUGUI statusText;
+
+	public void VRMode(){
+		statusText.SetText("VR Mode");
+		Destroy(this.gameObject);
+	}
+
+	public void FPSMode(){
+		statusText.SetText("FPS Mode");
+		VRSettings.LoadDeviceByName("None");
+		VRCharacter.SetActive(false);
+		FPSCharacter.SetActive(true);
+		Destroy(this.gameObject);
+	}
+
+	public void TrackerMode(){
+		statusText.SetText("Tracker Mode");
+		FindObjectOfType<ControllerStatsDisplay>().trackerMode = true;
+		Destroy(this.gameObject);
+	}
+}
