da0e0e36f59fdd5769409e873fd2b3ef3b2ed8a1
Daniel Tan <danieltan843@gmail.com>
2017-03-22 22:45:42 +1000

    Added Crank mechanic
    I think the crank works now. It’s a bit tough with the fp controller.
    Hopefully it’s fine with vr

---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/Driving/Driving.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour {
+
+	public GameObject steering;
+	public GameObject crank;
+
+	public float baseSpeed;
+	public float rotationSpeed;
+	float currentSpeed = 0.0f;
+
+
+	void Update() {
+//		Debug.Log ("Rotation: " + steering.transform.rotation.x);
+
+		if (steering.transform.rotation.eulerAngles.x > 5 && steering.transform.rotation.eulerAngles.x < 90) {
+//			Debug.Log ("Angle: " + steering.transform.rotation.eulerAngles.x);
+			this.transform.Rotate (new Vector3 (0f, rotationSpeed, 0f));
+		} else if (steering.transform.rotation.eulerAngles.x < 355 && steering.transform.rotation.eulerAngles.x > 270) {
+//			Debug.Log ("Angle: " + steering.transform.rotation.eulerAngles.x);
+			this.transform.Rotate (new Vector3 (0f, -rotationSpeed, 0f));
+		}
+
+		if (crank.transform.rotation.eulerAngles.z >= 5 && crank.transform.rotation.eulerAngles.z <= 50f) {
+			currentSpeed = baseSpeed * (Mathf.Clamp (crank.transform.rotation.eulerAngles.z, 0f, 30f) / 30f);
+//			Debug.Log ("speed: " + baseSpeed + " current: " + currentSpeed + " multiplier: " + (Mathf.Clamp (crank.transform.rotation.eulerAngles.z, 0f, 30f) / 30f));
+		} else if (crank.transform.rotation.eulerAngles.z <= 355 && crank.transform.rotation.eulerAngles.z >= 300) {
+			currentSpeed = baseSpeed * (Mathf.Clamp (crank.transform.rotation.eulerAngles.z - 360f, -30f, 0f) / 30f);
+
+//			Debug.Log ("multiplier: " + (Mathf.Clamp (crank.transform.rotation.eulerAngles.z - 360f, -30f, 0f) / 30f) + " -360: " + (crank.transform.rotation.eulerAngles.z - 360f));
+		} else {
+			currentSpeed = 0.0f;
+		}
+
+
+//		Debug.Log ("actual speed: " + Vector3.forward * currentSpeed);
+		this.transform.position += -this.transform.right * currentSpeed * Time.deltaTime;
+	}
+}
--- INB305 Unity/Assets/DT Test Stuff/Scripts/FPSInteract.cs
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/FPSInteract.cs
@@ -5,26 +5,16 @@ using UnityEngine;
 public class FPSInteract : MonoBehaviour {
 
 	public float reach;
-	public float rotationSpeed;
+
+	// Steering
 	public Steering steeringScript;
 
+	// Crank
+	public Crank crankScript;
+
 	// Raycasts
-	RaycastHit[] hits;
 	RaycastHit hit;
 	Ray ray;
-	Vector3 reference;
-	Vector3 currentPoint;
-	Vector3 rightOfRef;
-
-	Quaternion originalRotation;
-	Quaternion platformQ;
-	GameObject platform;
-
-	void Start() {
-		originalRotation = GameObject.FindGameObjectWithTag ("Steering").transform.rotation;
-		platform = GameObject.FindGameObjectWithTag ("Respawn");
-		platformQ = platform.transform.rotation;
-	}
 
 
 	void Update() {
@@ -34,8 +24,9 @@ public class FPSInteract : MonoBehaviour {
 			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			if (Physics.Raycast (ray, out hit, reach)) {
 				if (hit.transform.CompareTag ("Steering")) {
-//					reference = hit.point - hit.transform.position;
 					steeringScript.InitialInteractPoint (hit.point);
+				} else if (hit.transform.CompareTag ("Crank")) {
+					crankScript.InitialInteractPoint (hit.point);
 				}
 			}
 
@@ -46,33 +37,8 @@ public class FPSInteract : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit, reach)) {
 				if (hit.transform.CompareTag ("Steering")) {
 					steeringScript.PlayerRotating (hit.point);
-
-//					currentPoint = hit.point - hit.transform.position;
-//					rightOfRef = new Vector3 (reference.x, 0f, 1f);
-//
-//					rightOfRef.y = (0 - reference.z) / reference.y;
-//
-//					if (reference.y < 0) {
-//						rightOfRef *= -1;
-//					}
-//
-//					float angleRef = Vector3.Angle (rightOfRef, reference);
-//					float angleCur = Vector3.Angle (rightOfRef, currentPoint);
-//
-//					hit.transform.RotateAround (hit.transform.position, hit.transform.right, angleRef - angleCur);
-////					hit.transform.localRotation.SetFromToRotation(reference, currentPoint);
-//
-//					reference = currentPoint;
-				}
-			}
-		}
-
-		if (Input.GetMouseButtonUp (0)) {
-			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit, reach)) {
-				if (hit.transform.CompareTag ("Steering")) {
-//					hit.transform.localRotation.SetFromToRotation (hit.transform.localRotation.eulerAngles, Vector3.zero);
-//					hit.transform.rotation = new Quaternion(originalRotation.x, platform.transform.rotation.y, platform.transform.rotation.z, platform.transform.rotation.w) ;
+				} else if (hit.transform.CompareTag ("Crank")) {
+					crankScript.PlayerRotating (hit.point);
 				}
 			}
 		}
