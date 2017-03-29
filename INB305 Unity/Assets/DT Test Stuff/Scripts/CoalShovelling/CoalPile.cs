bc1e7747b8194b2eca58067666d7365d885c1145
Daniel Tan <danieltan843@gmail.com>
2017-03-29 19:18:57 +1000

    Added Shovel, some coal and a furnace

---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/CoalPile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoalPile : MonoBehaviour {
+
+	public Shovel shovelScript;
+	public float giveMultiplier = 0.5f;
+	public float minVelocty = 1.5f;
+	float giveAmount = 0.0f;
+	float magnitude = 0.0f;
+
+	Vector3 vel = new Vector3();
+	Vector3 projection = new Vector3();
+	Vector3 otherPos = new Vector3();
+	float angle = 0.0f;
+
+	void Update() {
+		Debug.DrawLine (projection, this.transform.position, Color.blue);
+		Debug.DrawLine (otherPos, this.transform.position, Color.red);
+		Debug.DrawLine (otherPos, vel.normalized, Color.green);
+//		Debug.DrawRay (otherPos, vel, Color.green);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Shovel")) {
+			otherPos = other.transform.position;
+			vel = shovelScript.velocity;
+			projection = new Vector3 (other.transform.position.x, this.transform.position.y, other.transform.position.z);
+			Debug.Log ("projection: " + Vector3.Angle (projection, this.transform.up));
+			float angle = Vector3.Angle (projection, other.transform.position - shovelScript.velocity);
+			Debug.Log ("Shovel angle: " + angle);
+
+			magnitude = shovelScript.velocity.magnitude * giveMultiplier;
+			Debug.Log("initial vel: " + shovelScript.velocity.magnitude + " with multiplier: " + magnitude);
+			giveAmount = Mathf.Clamp(magnitude, 0, shovelScript.maxAmount);
+			if (giveAmount > shovelScript.coalAmount && magnitude > minVelocty) {
+				shovelScript.coalAmount = giveAmount;
+			}
+		}
+	}
+}
---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/Shovel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shovel : MonoBehaviour {
+
+	public float coalAmount = 0.0f;
+	public float maxAmount;
+
+	Vector3 prev = new Vector3();
+	Vector3 next = new Vector3();
+	public Vector3 velocity = new Vector3();
+
+
+	void Start() {
+		next = this.transform.position;
+	}
+
+	void Update() {
+
+		CalculateVelocity ();
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			coalAmount = 0.0f;
+		}
+	}
+
+	void CalculateVelocity() {
+		prev = next;
+		next = this.transform.position;
+
+		velocity = (next - prev) / Time.deltaTime;
+	}
+}
