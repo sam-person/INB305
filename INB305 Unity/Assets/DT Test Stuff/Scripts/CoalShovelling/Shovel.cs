61750dcdf606240dc5f84f0bac50cc6d1611819b
Daniel Tan <danieltan843@gmail.com>
2017-04-08 23:33:38 +1000

    Added coal loss to Shovel
    - Shovel can lose coal if not held right

--- INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/Shovel.cs
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/Shovel.cs
@@ -8,8 +8,14 @@ public class Shovel : MonoBehaviour {
 	public float maxAmount = 20f;
 
 	public bool isUpsideDown = false;
+	float lossRate = 4f;
+	Vector2 Xmin = new Vector2 (320f, 40f);
+	Vector2 Xmax = new Vector2 (290f, 70f);
 
+	Vector2 Zmin = new Vector2 (335f, 25f);
+	Vector2 Zmax = new Vector2 (280f, 80f);
 
+	// Velocity
 	Vector3 prev = new Vector3();
 	Vector3 next = new Vector3();
 	public Vector3 velocity = new Vector3();
@@ -48,21 +54,36 @@ public class Shovel : MonoBehaviour {
 	}
 
 	void CheckXZAngle() {
-		if (65f > this.transform.eulerAngles.y || this.transform.eulerAngles.y > (360f - 65)) {
+		float angle = 0.0f;
+		if (Xmin.x > this.transform.eulerAngles.x && this.transform.eulerAngles.x > Xmin.y) {
 			// lose coal based on angle amount
 			// need to get angle and take from boundaries
 			// then divide by something, multiply by time.delta (implies timer)
 			// then take from coal amount
-//			LoseCoal (1);
-		} else if (300 > this.transform.eulerAngles.z && this.transform.eulerAngles.z > 230f) {
-//			LoseCoal (1);
-		}
-	}
 
-	void CheckYAngle() {
-		if (80f > this.transform.eulerAngles.y || this.transform.eulerAngles.y > (360f - 80)) {
-			isUpsideDown = true;
-			LoseCoal (maxAmount);
+			if (Xmax.x > this.transform.eulerAngles.x && this.transform.eulerAngles.x > Xmax.y) {
+				LoseCoal (lossRate * Time.deltaTime);
+			} else {
+				angle = this.transform.eulerAngles.x;
+				if (angle > 180f) {
+					angle = Mathf.Abs (angle - 360f);
+				}
+
+				LoseCoal (lossRate * angle / Xmax.y * Time.deltaTime);
+			}
+
+		} else if (Zmin.x > this.transform.eulerAngles.z && this.transform.eulerAngles.z > Zmin.y) {
+
+			if (Zmax.x > this.transform.eulerAngles.z && this.transform.eulerAngles.z > Zmax.y) {
+				LoseCoal (lossRate * Time.deltaTime);
+			} else {
+				angle = this.transform.eulerAngles.z;
+				if (angle > 180f) {
+					angle = Mathf.Abs (angle - 360f);
+				}
+
+				LoseCoal (lossRate * angle / Zmax.y * Time.deltaTime);
+			}
 		}
 	}
 
@@ -77,8 +98,7 @@ public class Shovel : MonoBehaviour {
 
 	// coal falling out of shovel
 	void LoseCoal(float amount) {
-		Debug.Log ("Losing coal: " + amount);
-		coalAmount -= amount;
-		coalAmount = Mathf.Clamp (coalAmount, 0f, maxAmount);
+//		Debug.Log ("Losing coal: " + amount);
+		coalAmount = Mathf.Clamp (coalAmount - amount, 0f, maxAmount);
 	}
 }
