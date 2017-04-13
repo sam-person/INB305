3735b1279c304ad75aedebb2e0b348a22fb5c054
Daniel Tan <danieltan843@gmail.com>
2017-04-13 14:39:58 +1000

    Started shovel coal

---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/CoalOnShovel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoalOnShovel : MonoBehaviour {
+
+	BoxCollider boxCollider;
+	Vector3 initialScale = new Vector3();
+	Vector3 currentScale = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		boxCollider = GetComponent<BoxCollider> ();
+		initialScale = transform.localScale;
+		currentScale = initialScale;
+	}
+	
+	public void SetScale(float scale) {
+//		currentScale = 
+	}
+}
