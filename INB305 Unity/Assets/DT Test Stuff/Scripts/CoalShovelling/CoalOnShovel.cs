c2337cb39d8d244c7a82a1509228a8f7f91bddb2
Daniel Tan <danieltan843@gmail.com>
2017-04-13 22:49:40 +1000

    Shovel now has coal on it
    - yay .meta is back

--- INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/CoalOnShovel.cs
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/CoalShovelling/CoalOnShovel.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class CoalOnShovel : MonoBehaviour {
 
 	BoxCollider boxCollider;
-	Vector3 initialScale = new Vector3();
+	Vector3 maxScale = new Vector3();
 	Vector3 currentScale = new Vector3();
+
+//	public GameObject shovelCoal;
+	public MeshRenderer shovelCoalRenderer;
+//	Color originalColor;
 
 	// Use this for initialization
 	void Start () {
 		boxCollider = GetComponent<BoxCollider> ();
-		initialScale = transform.localScale;
-		currentScale = initialScale;
+		maxScale = transform.localScale;
+		currentScale = maxScale;
+
+//		meshRenderer = shovelCoal.GetComponent<MeshRenderer> ();
+//		Debug.Log (meshRenderer);
+//		originalColor = meshRenderer.material.color;
+
+		SetScale (0f);
 	}
 	
 	public void SetScale(float scale) {
 		currentScale.y = scale;
 		this.transform.localScale = currentScale;
+		if (currentScale.y < 0.02f) {
+//			originalColor.a = 0f;
+			shovelCoalRenderer.enabled = false;
+		} else {
+//			originalColor.a = 1f;
+			shovelCoalRenderer.enabled = true;
+		}
+//		meshRenderer.material.color = originalColor;
 	}
 }
