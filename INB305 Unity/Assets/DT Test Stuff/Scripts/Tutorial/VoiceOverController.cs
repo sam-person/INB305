cfbd9af77f365959aa9a6e7aa75959bccf93a449
Vaconcovat <n9173820@qut.edu.au>
2017-06-01 15:10:43 +1000

    Added some tutorial stuff (DT)

---
+++ INB305 Unity/Assets/DT Test Stuff/Scripts/Tutorial/VoiceOverController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceOverController : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	public AudioClip[] voiceOverClips;
+
+	public AudioClip startVoiceOver;
+
+	public AudioClip[] controlClips;
+
+	Tutorial tutorialScript;
+
+	void Start() {
+		tutorialScript = GetComponent<Tutorial> ();
+	}
+
+	public void PlayClipAtStage(int stage) {
+		switch (stage) {
+		case 1:
+			PlayAudioClip (startVoiceOver);
+			break;
+		}
+	}
+
+	void PlayAudioClip(AudioClip clip) {
+		audioSource.Stop ();
+		audioSource.clip = clip;
+		audioSource.Play ();
+		StopCoroutine (WaitForClip (clip));
+		StartCoroutine (WaitForClip (clip));
+		tutorialScript.audioSourcePlaying = true;
+	}
+
+	IEnumerator WaitForClip(AudioClip clip) {
+		yield return new WaitForSeconds (clip.length);
+
+		tutorialScript.AudioDonePlaying ();
+	}
+}
