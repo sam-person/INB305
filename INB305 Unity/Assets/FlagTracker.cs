958ea89d23224246005303a90146bdaa4ab4a79c
Vaconcovat <vaconcovat@gmail.com>
2017-07-31 12:35:42 +1000

    Added controller events listener, setup mode
    game is in setup mode by default and tracks flags marked by the right
    hand controller

---
+++ INB305 Unity/Assets/FlagTracker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class FlagTracker : MonoBehaviour {
+
+	public bool setup = false;
+
+	public List<Vector3> flags;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<VRTK_ControllerEvents>().TriggerUnclicked += new ControllerInteractionEventHandler(DoTriggerUnClicked);
+	}
+
+	void OnDrawGizmos () {
+		foreach(Vector3 f in flags){
+			Gizmos.DrawSphere(f, 0.1f);
+		}
+	}
+
+	private void DoTriggerUnClicked(object sender, ControllerInteractionEventArgs e){
+		if(!setup){
+			return;
+		}
+
+		Debug.Log("Placing a flag at " + transform.position.ToString());
+		flags.Add(transform.position);
+
+
+
+    }
+}
