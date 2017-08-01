e54caa8372a4d666b7c3348592a3a1515edee799
Vaconcovat <vaconcovat@gmail.com>
2017-08-01 23:10:59 +1000

    Flag tracker setup

--- INB305 Unity/Assets/FlagTracker.cs
+++ INB305 Unity/Assets/FlagTracker.cs
@@ -9,14 +9,27 @@ public class FlagTracker : MonoBehaviour {
 
 	public List<Vector3> flags;
 
+	public Vector3 lowerBound, higherBound;
+
+	public GameObject tracker;
+
 	// Use this for initialization
 	void Start () {
 		GetComponent<VRTK_ControllerEvents>().TriggerUnclicked += new ControllerInteractionEventHandler(DoTriggerUnClicked);
 	}
 
 	void OnDrawGizmos () {
+		Gizmos.color = Color.blue;
 		foreach(Vector3 f in flags){
-			Gizmos.DrawSphere(f, 0.1f);
+			Gizmos.DrawSphere(f, 0.05f);
+		}
+
+		if(flags.Count >= 2){
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(lowerBound, new Vector3(lowerBound.x, 0, higherBound.z));
+			Gizmos.DrawLine(lowerBound, new Vector3(higherBound.x, 0, lowerBound.z));
+			Gizmos.DrawLine(higherBound, new Vector3(lowerBound.x, 0, higherBound.z));
+			Gizmos.DrawLine(higherBound, new Vector3(higherBound.x, 0, lowerBound.z));
 		}
 	}
 
@@ -25,10 +38,61 @@ public class FlagTracker : MonoBehaviour {
 			return;
 		}
 
-		Debug.Log("Placing a flag at " + transform.position.ToString());
-		flags.Add(transform.position);
+
+		AddFlag(transform.position);
+		GetComponent<VRTK_ControllerActions>().TriggerHapticPulse(1,0.5f,0.01f);
+
+		Debug.Log("Placing a flag at " + transform.position.ToString() + " | Relative position: " + GetPercentagePosition(transform.position).ToString());
 
 
+    }
 
+    void AddFlag(Vector3 position){
+		flags.Add(position);
+		if(flags.Count >= 2){
+			CalculateBounds();
+		}
+    }
+
+    void CalculateBounds(){
+    	float x, z; 
+
+		//lower bounds
+    	x = Mathf.Infinity;
+    	z = Mathf.Infinity;
+    	foreach(Vector3 v in flags){
+    		if(v.x < x){
+    			x = v.x;
+    		}
+    		if(v.z < z){
+    			z = v.z;
+    		}
+    	}
+    	lowerBound = new Vector3(x-0.5f,0,z-0.5f);
+
+		//higher bounds
+    	x = Mathf.NegativeInfinity;
+    	z = Mathf.NegativeInfinity;
+    	foreach(Vector3 v in flags){
+    		if(v.x > x){
+    			x = v.x;
+    		}
+    		if(v.z > z){
+    			z = v.z;
+    		}
+    	}
+    	higherBound = new Vector3(x+0.5f,0,z+0.5f);
+
+    }
+
+    public Vector3 GetPercentagePosition(Vector3 input){
+    	if(flags.Count < 2){
+    		return Vector3.zero;
+    	}
+    	else{
+    		float x = Mathf.InverseLerp(lowerBound.x, higherBound.x, input.x);
+			float z = Mathf.InverseLerp(lowerBound.z, higherBound.z, input.z);
+			return new Vector3(x, input.y, z);
+    	}
     }
 }
--- INB305 Unity/Assets/UI_Manager.cs
+++ INB305 Unity/Assets/UI_Manager.cs
@@ -6,7 +6,15 @@ public class UI_Manager : MonoBehaviour {
 
 	public GameObject UI_Panel;
 	public TutorialManager tutorial;
-	public TMPro.TextMeshProUGUI statusText, tutorialstageText;
+	public TMPro.TextMeshProUGUI statusText, tutorialstageText, toggleFlagText;
+	public FlagTracker flagTracker;
+
+	public List<RectTransform> UI_Flags;
+
+	public RectTransform minimapPanel;
+	public GameObject flagPrefab;
+
+	public Vector2 MinimapSize;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +27,10 @@ public class UI_Manager : MonoBehaviour {
 			UI_Panel.SetActive(!UI_Panel.activeSelf);
 		}
 
+		if(UI_Flags.Count - 1 < flagTracker.flags.Count){
+			UI_Flags.Add(Instantiate(flagPrefab, minimapPanel).GetComponent<RectTransform>());
+		}
+
 		if(tutorial.tutorialStage >= 0){
 			tutorialstageText.text = "Stage: " + tutorial.tutorialStage;
 		}
@@ -26,14 +38,48 @@ public class UI_Manager : MonoBehaviour {
 			tutorialstageText.text = "";
 		}
 
+		if(flagTracker.setup){
+			toggleFlagText.text = "Setup Active.\n" + flagTracker.flags.Count + " Flags Registered.";
+		}
+		else{
+			toggleFlagText.text = "Setup Inactive.\n" + flagTracker.flags.Count + " Flags Registered.";
+		}
+
+		UI_Flags[0].anchoredPosition = getRelativePosition(flagTracker.tracker.transform.position);
+
+		for(int i = 1; i < UI_Flags.Count; i++){
+			UI_Flags[i].anchoredPosition = getRelativePosition(flagTracker.flags[i-1]);
+		}
+
+	}
+
+	void DisplayMessage(string message){
+		statusText.text = message;
 	}
 
 	public void StartTutorialButton(){
 		if(tutorial.StartTutorial()){
-			statusText.text = "Tutorial started successfully!";
+			DisplayMessage("Tutorial started successfully!");
 		}
 		else{
-			statusText.text = "Tutorial failed to start - is tutorial already running?";
+			DisplayMessage("Tutorial failed to start - is tutorial already running?");
 		}
+	}
+
+	public void ToggleFlagButton(){
+		flagTracker.setup = !flagTracker.setup;
+		if(flagTracker.setup){
+			DisplayMessage("Setup Active. Pull the trigger on right hand controller to add flags.");
+		}
+		else{
+			DisplayMessage("Setup Disabled.");
+		}
+	}
+
+	Vector2 getRelativePosition(Vector3 input){
+		float x, y;
+		x = Mathf.Lerp(0,MinimapSize.x,flagTracker.GetPercentagePosition(input).x);
+		y = Mathf.Lerp(0,MinimapSize.y,flagTracker.GetPercentagePosition(input).z);
+		return new Vector2(x,y);
 	}
 }
